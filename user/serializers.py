2ce9ad0b1fe83b7cc8bbaa4d3903a83abbdeac82
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-01 17:43:51 +0900

    add: UserSerializer

---
+++ user/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        read_only_fields = ("id",)
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
