897ee318ce3a7f8d2ec21bf97ee6a663b22e809f
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-02 17:51:01 +0900

    add: Room Model

--- pms/models.py
+++ pms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Room(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
+    floor = models.CharField(max_length=10, null=True, blank=True)
+    room_number = models.CharField(max_length=20, null=True, blank=True)
+    type = models.CharField(max_length=20, null=True, blank=True)
+    status = models.CharField(max_length=20, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
