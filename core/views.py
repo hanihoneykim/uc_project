3a5494d22854e21dbef421f743980061888517a5
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-02 10:25:40 +0900

    chore: organizing folders

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     user = request.user
-    return render(request, "index.html", {"user": user})
+    return render(request, "pages/index.html", {"user": user})
